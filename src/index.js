c0c5ebaf8aee12c8e8d1c16f486c2748e4c980de
Nico <nicogreco4k@gmail.com>
2018-01-21 14:04:45 -0500

    Clock class

--- src/index.js
+++ src/index.js
@@ -18,4 +18,21 @@ Clock.prototype.timeUntilNextYear = function() {
 	//TODO...
 };
 
+Clock.prototype.daylightSavings = function() {
+	//TODO...
+};
+
+Clock.prototype.date = function(args) {
+	return new Date(args.year, args.month, args.day);
+};
+
+Clock.prototype.utc = function() {
+	
+};
+
+// Alias for utc
+Clock.prototype.gmt = function() {
+	return this.utc();
+};
+
 module.exports = Clock;
