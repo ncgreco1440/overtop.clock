e6b535c84efdc0e9686174a5652d409d1d14d078
Nico <nicogreco4k@gmail.com>
2018-01-21 13:02:18 -0500

    Clock class

---
+++ src/index.js
@@ -0,0 +1,21 @@
+var Clock = function() {
+	
+};
+
+Clock.prototype.timeUntilMidnight = function() {
+	//TODO...
+};
+
+Clock.prototype.timeUntilNextWeek = function() {
+	//TODO...
+};
+
+Clock.prototype.timeUntilNextMonth = function() {
+	//TODO...
+};
+
+Clock.prototype.timeUntilNextYear = function() {
+	//TODO...
+};
+
+module.exports = Clock;
